8795488cd055f69c0c2797d92711bc08601b81c2
MB Takim <mberk.takim@gmail.com>
2025-04-07 21:15:46 +0200

    Toggle theme using async storage

--- stores/themeStore.tsx
+++ stores/themeStore.tsx
@@ -1,5 +1,16 @@
 import { observable } from '@legendapp/state';
+import { syncObservable } from '@legendapp/state/sync';
+import persistOptions from './persistConfig';
 
 export const themeStore = observable({
   mode: 'system' as 'light' | 'dark' | 'system',
 });
+
+syncObservable(
+  themeStore,
+  persistOptions({
+    persist: {
+      name: 'themeStore',
+    },
+  }),
+);
