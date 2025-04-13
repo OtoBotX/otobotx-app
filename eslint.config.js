a43715a32db1ec632b2d1be442fcfadaa4bd1d18
MB Takim <mberk.takim@gmail.com>
2025-04-13 23:40:25 +0200

    Fixed eslint and package.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,38 @@
 import js from '@eslint/js'
-import tseslint from 'typescript-eslint'
 import prettier from 'eslint-config-prettier'
+import reactPlugin from 'eslint-plugin-react'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 
-export default await tseslint.config({
-  files: ['**/*.{ts,tsx,js,jsx}'],
-  languageOptions: {
-    parserOptions: {
-      tsconfigPath: './tsconfig.json',
+export default [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+        project: './tsconfig.json', 
+        tsconfigRootDir: process.cwd(),
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'no-console': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  plugins: {
-    react: require('eslint-plugin-react'),
-  },
-  rules: {
-    ...js.configs.recommended.rules,
-    ...tseslint.configs.recommended.rules,
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'no-console': 'warn',
-  },
-}).then((baseConfig) => [
-  ...baseConfig,
   prettier,
-])
+]
