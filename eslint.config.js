6df0cbe1caec39cd8e60ada58589769a16de0191
MB Takim <mberk.takim@gmail.com>
2025-04-13 23:34:10 +0200

    fix unnecessary import

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-config-prettier'
-import reactPlugin from 'eslint-plugin-react'
 
 export default await tseslint.config({
   files: ['**/*.{ts,tsx,js,jsx}'],
