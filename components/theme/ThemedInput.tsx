6aa74290642692d19c46fa7a3768cd9fa95cd992
MB Takim <mberk.takim@gmail.com>
2025-04-19 02:04:52 +0200

    themed input prop mismatch fixed

--- components/theme/ThemedInput.tsx
+++ components/theme/ThemedInput.tsx
@@ -6,13 +6,13 @@ type Props = React.ComponentProps<typeof TextInput> & {
   containerStyle?: StyleProp<ViewStyle>;
 };
 
-export default function ThemedInput({ containerStyle, ...props }: Props) {
+export default function ThemedInput( props: Props) {
   const theme = useTheme();
 
   return (
     <TextInput
       mode="outlined"
-      style={[{ marginBottom: 12 }, containerStyle]}
+      style={{ marginBottom: 12 }}
       outlineColor={theme.colors.outline}
       activeOutlineColor={theme.colors.primary}
       theme={{ colors: { text: theme.colors.onSurface } }}
