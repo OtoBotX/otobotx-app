ecd59aa2cb1c546daad9af1c5e89bb4bd4876159
MB Takim <mberk.takim@gmail.com>
2025-04-20 23:42:14 +0200

    from index to settings tab

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,6 +1,34 @@
-import ThemedText from "@/components/theme/ThemedText";
 import ThemedView from "@/components/theme/ThemedView";
+import ThemedText from "@/components/theme/ThemedText";
+import ThemedButton from "@/components/theme/ThemedButton";
+import { useThemeHandler } from "@/hooks/useThemeHandler";
+import { useLangHandler } from "@/hooks/useLangHandler";
+import { useUserHandler } from "@/hooks/useUserHandler";
+import { t } from "@/i18n/t";
 
 export default function SettingsScreen() {
-  return <ThemedView><ThemedText type="title" style={{ textAlign: "center" }}>Settings Screen</ThemedText></ThemedView>;
+  const { toggleTheme } = useThemeHandler();
+  const { toggleLang } = useLangHandler();
+  const { handleLogout } = useUserHandler();
+
+  return (
+    <ThemedView style={{ flex: 1, justifyContent: "center", padding: 24 }}>
+      <ThemedText type="title" style={{ marginBottom: 24 }}>
+        {t("settings.title")}
+      </ThemedText>
+
+      <ThemedButton onPress={toggleTheme}>
+        {t("settings.toggleTheme")}
+      </ThemedButton>
+
+      <ThemedButton onPress={toggleLang}>
+        {t("settings.toggleLang")}
+      </ThemedButton>
+
+      <ThemedButton onPress={handleLogout} mode="outlined">
+        {t("settings.logout")}
+      </ThemedButton>
+
+    </ThemedView>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,9 @@ import ThemedButton from "@/components/theme/ThemedButton";
 import AuthView from "@/components/auth/AuthView";
 import ThemedText from "@/components/theme/ThemedText";
 import { router } from "expo-router";
-import { useThemeHandler } from "@/hooks/useThemeHandler";
-import { useLangHandler } from "@/hooks/useLangHandler";
 import { t } from "@/i18n/t";
 
 export default function HomeScreen() {
-  const { toggleTheme } = useThemeHandler();
-  const { toggleLang } = useLangHandler();
 
   const goToRegister = () => router.push("/register");
 
@@ -16,17 +12,10 @@ export default function HomeScreen() {
     <AuthView>
       <ThemedText type="title">{t("onboard.welcome")}</ThemedText>
 
-      <ThemedButton mode="contained" onPress={toggleTheme}>
-        {t("onboard.toggleTheme")}
-      </ThemedButton>
-
-      <ThemedButton mode="contained" onPress={toggleLang}>
-        {t("onboard.toggleLanguage")}
-      </ThemedButton>
-
       <ThemedButton mode="outlined" onPress={goToRegister}>
         {t("onboard.registerlogin")}
       </ThemedButton>
+
     </AuthView>
   );
 }
