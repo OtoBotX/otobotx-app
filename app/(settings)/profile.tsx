1a6021fd63c2b0febd4a1682f3e04f7dde9c3af7
MB Takim <mberk.takim@gmail.com>
2025-05-12 00:21:45 +0200

    created a profile settings page

--- app/(settings)/profile.tsx
+++ app/(settings)/profile.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import SettingsView from '@/components/settings/SettingsView';
 import { t } from '@/i18n/t';
-import { Text } from 'react-native-paper';
+import ThemedInput from '@/components/theme/ThemedInput';
+import { useUserHandler } from '@/hooks/useUserHandler';
 
 export default function ProfilePage() {
+
+const {
+    email,
+    first_name,
+    last_name,
+  } = useUserHandler();
+
   return (
     <SettingsView withBack title={t('settings.profile')}>
-      <Text>Hey</Text>
+      <ThemedInput label={t('auth.email')} value={email} editable={false} />
+      <ThemedInput label={t('auth.firstName')} value={first_name} editable={false} />
+      <ThemedInput label={t('auth.lastName')} value={last_name} editable={false} />
     </SettingsView>
   );
 }
